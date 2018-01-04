e40084f4a16247d3c150fcb215359144e8e8c98f
Brian Heacook <heacookb@protonmail.com>
2018-01-04 13:30:18 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array)
+{
+  for(let i = 0; i<25; i++)
+  {
+    array.push("I am" + i + " strange loop")
+  }
+  return array;
+}
+function whileLoop(num)
+{
+    while(num >= 0)
+    {
+       console.log(num)
+       num--;
+    }
+}
+function doWhileLoop(array)
+{
+  do{
+    
+  }
+}
